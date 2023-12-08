802454e2ec6dfc5c77c4a8bebb09073da454c9f2
Błażej Kapała <contact@green.media.pl>
2023-12-08 15:32:36 +0100

    Create print.ps1

---
+++ print.ps1
@@ -0,0 +1,21 @@
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+
+$screens = [Windows.Forms.Screen]::AllScreens
+
+$top    = ($screens.Bounds.Top    | Measure-Object -Minimum).Minimum
+$left   = ($screens.Bounds.Left   | Measure-Object -Minimum).Minimum
+$right  = ($screens.Bounds.Right  | Measure-Object -Maximum).Maximum
+$bottom = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum
+
+$bounds   = [Drawing.Rectangle]::FromLTRB($left, $top, $right, $bottom)
+$bmp      = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height)
+$graphics = [Drawing.Graphics]::FromImage($bmp)
+
+$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+# $bmp.Save("$env:USERPROFILE\test.png")
+# add timestamp to filename
+$bmp.Save("$env:USERNAME-$(Get-Date -Format 'yyyyMMddHHmmss').png")
+
+$graphics.Dispose()
+$bmp.Dispose()
